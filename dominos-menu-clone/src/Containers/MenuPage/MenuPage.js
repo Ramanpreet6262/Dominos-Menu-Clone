b2b91108ca25a5f39df741860eac2b1a1ed41b32
Ramanpreet6262 <ramanpreet6262@gmail.com>
2020-03-02 08:46:48 +0530

    follow up

--- dominos-menu-clone/src/Containers/MenuPage/MenuPage.js
+++ dominos-menu-clone/src/Containers/MenuPage/MenuPage.js
@@ -7,14 +7,16 @@ class MenuPage extends Component {
   render() {
     return (
       <React.Fragment>
-        <div className='menuPage'>
+        <div className='menuPage flex-container'>
           <div
             className='card'
             style={{
               width: '23rem',
               border: '1px solid #fff',
               boxShadow:
-                'rgba(0, 0, 0, 0.12) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 2px 4px 0px'
+                'rgba(0, 0, 0, 0.12) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 2px 4px 0px',
+              marginBottom: '40px',
+              marginRight: '30px'
             }}
           >
             <img
@@ -43,6 +45,208 @@ class MenuPage extends Component {
               </button>
             </div>
           </div>
+
+
+          <div
+            className='card'
+            style={{
+              width: '23rem',
+              border: '1px solid #fff',
+              boxShadow:
+                'rgba(0, 0, 0, 0.12) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 2px 4px 0px',
+              marginBottom: '40px',
+              marginRight: '30px'
+            }}
+          >
+            <img
+              src={peppyPaneer}
+              className='card-img-top'
+              alt='Peppy Paneer Pizza'
+            />
+            <div className='card-body'>
+              <div className='title-div'>
+                <h5 className='card-title title-head'>Peppy Paneer &#8377;</h5>
+                <p className='card-text title-subhead'>
+                  Flavorful trio of juicy paneer, crisp capsicum with spicy red
+                  paprika
+                </p>
+              </div>
+              <button
+                type='button'
+                className='btn btn-outline-success float-right'
+                style={{
+                  fontWeight: 'bold',
+                  fontSize: '0.75rem',
+                  padding: '.375rem .5rem'
+                }}
+              >
+                ADD TO CART
+              </button>
+            </div>
+          </div>
+
+
+
+          <div
+            className='card'
+            style={{
+              width: '23rem',
+              border: '1px solid #fff',
+              boxShadow:
+                'rgba(0, 0, 0, 0.12) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 2px 4px 0px',
+              marginBottom: '40px',
+              marginRight: '30px'
+            }}
+          >
+            <img
+              src={peppyPaneer}
+              className='card-img-top'
+              alt='Peppy Paneer Pizza'
+            />
+            <div className='card-body'>
+              <div className='title-div'>
+                <h5 className='card-title title-head'>Peppy Paneer &#8377;</h5>
+                <p className='card-text title-subhead'>
+                  Flavorful trio of juicy paneer, crisp capsicum with spicy red
+                  paprika
+                </p>
+              </div>
+              <button
+                type='button'
+                className='btn btn-outline-success float-right'
+                style={{
+                  fontWeight: 'bold',
+                  fontSize: '0.75rem',
+                  padding: '.375rem .5rem'
+                }}
+              >
+                ADD TO CART
+              </button>
+            </div>
+          </div>
+
+
+
+          <div
+            className='card'
+            style={{
+              width: '23rem',
+              border: '1px solid #fff',
+              boxShadow:
+                'rgba(0, 0, 0, 0.12) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 2px 4px 0px',
+              marginBottom: '40px',
+              marginRight: '30px'
+            }}
+          >
+            <img
+              src={peppyPaneer}
+              className='card-img-top'
+              alt='Peppy Paneer Pizza'
+            />
+            <div className='card-body'>
+              <div className='title-div'>
+                <h5 className='card-title title-head'>Peppy Paneer &#8377;</h5>
+                <p className='card-text title-subhead'>
+                  Flavorful trio of juicy paneer, crisp capsicum with spicy red
+                  paprika
+                </p>
+              </div>
+              <button
+                type='button'
+                className='btn btn-outline-success float-right'
+                style={{
+                  fontWeight: 'bold',
+                  fontSize: '0.75rem',
+                  padding: '.375rem .5rem'
+                }}
+              >
+                ADD TO CART
+              </button>
+            </div>
+          </div>
+
+
+
+          <div
+            className='card'
+            style={{
+              width: '23rem',
+              border: '1px solid #fff',
+              boxShadow:
+                'rgba(0, 0, 0, 0.12) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 2px 4px 0px',
+              marginBottom: '40px',
+              marginRight: '30px'
+            }}
+          >
+            <img
+              src={peppyPaneer}
+              className='card-img-top'
+              alt='Peppy Paneer Pizza'
+            />
+            <div className='card-body'>
+              <div className='title-div'>
+                <h5 className='card-title title-head'>Peppy Paneer &#8377;</h5>
+                <p className='card-text title-subhead'>
+                  Flavorful trio of juicy paneer, crisp capsicum with spicy red
+                  paprika
+                </p>
+              </div>
+              <button
+                type='button'
+                className='btn btn-outline-success float-right'
+                style={{
+                  fontWeight: 'bold',
+                  fontSize: '0.75rem',
+                  padding: '.375rem .5rem'
+                }}
+              >
+                ADD TO CART
+              </button>
+            </div>
+          </div>
+
+
+
+          <div
+            className='card'
+            style={{
+              width: '23rem',
+              border: '1px solid #fff',
+              boxShadow:
+                'rgba(0, 0, 0, 0.12) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 2px 4px 0px',
+              marginBottom: '40px',
+              marginRight: '30px'
+            }}
+          >
+            <img
+              src={peppyPaneer}
+              className='card-img-top'
+              alt='Peppy Paneer Pizza'
+            />
+            <div className='card-body'>
+              <div className='title-div'>
+                <h5 className='card-title title-head'>Peppy Paneer &#8377;</h5>
+                <p className='card-text title-subhead'>
+                  Flavorful trio of juicy paneer, crisp capsicum with spicy red
+                  paprika
+                </p>
+              </div>
+              <button
+                type='button'
+                className='btn btn-outline-success float-right'
+                style={{
+                  fontWeight: 'bold',
+                  fontSize: '0.75rem',
+                  padding: '.375rem .5rem'
+                }}
+              >
+                ADD TO CART
+              </button>
+            </div>
+          </div>
+
+          
+
         </div>
       </React.Fragment>
     );
