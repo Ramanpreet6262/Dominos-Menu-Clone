724cc0fe62448fd31d4157a0c663a8ec49082ccf
Ramanpreet6262 <ramanpreet6262@gmail.com>
2020-03-02 16:13:02 +0530

    added data and empty cart design

--- dominos-menu-clone/src/Containers/MenuPage/MenuPage.js
+++ dominos-menu-clone/src/Containers/MenuPage/MenuPage.js
@@ -7,7 +7,7 @@ import nonvegMarker from '../../static/assets/non_veg.svg';
 import fav from '../../static/assets/favorite.svg';
 import fillfav from '../../static/assets/fill_favorite.svg';
 import customiseArrow from '../../static/assets/customise_arrow.svg';
-
+import emptyCart from '../../static/assets/empty_cart.png';
 
 class MenuPage extends Component {
   render() {
@@ -30,7 +30,7 @@ class MenuPage extends Component {
               className='card-img-top'
               alt='Peppy Paneer Pizza'
             />
-            <div class="card-img-overlay">
+            <div className="card-img-overlay">
               <img src={vegMarker} className='category-marker' alt='veg' />
               <span className='pizza-price'>&#8377; 215</span>
               <button className='customise-button'>CUSTOMISE <img src={customiseArrow} className='customise-arrow' alt='arrow' /></button>
@@ -57,47 +57,7 @@ class MenuPage extends Component {
               </button>
             </div>
           </div>
-
 {/* 
-          <div
-            className='card'
-            style={{
-              width: '23rem',
-              border: '1px solid #fff',
-              boxShadow:
-                'rgba(0, 0, 0, 0.12) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 2px 4px 0px',
-              marginBottom: '40px',
-              marginRight: '30px'
-            }}
-          >
-            <img
-              src={peppyPaneer}
-              className='card-img-top'
-              alt='Peppy Paneer Pizza'
-            />
-            <div className='card-body'>
-              <div className='title-div'>
-                <h5 className='card-title title-head'>Peppy Paneer &#8377;</h5>
-                <p className='card-text title-subhead'>
-                  Flavorful trio of juicy paneer, crisp capsicum with spicy red
-                  paprika
-                </p>
-              </div>
-              <button
-                type='button'
-                className='btn btn-outline-success float-right'
-                style={{
-                  fontWeight: 'bold',
-                  fontSize: '0.75rem',
-                  padding: '.375rem .5rem'
-                }}
-              >
-                ADD TO CART
-              </button>
-            </div>
-          </div>
-
-
 
           <div
             className='card'
@@ -222,6 +182,46 @@ class MenuPage extends Component {
           <div
             className='card'
             style={{
+              width: '23rem',
+              border: '1px solid #fff',
+              boxShadow:
+                'rgba(0, 0, 0, 0.12) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 2px 4px 0px',
+              marginBottom: '40px',
+              marginRight: '30px'
+            }}
+          >
+            <img
+              src={peppyPaneer}
+              className='card-img-top'
+              alt='Peppy Paneer Pizza'
+            />
+            <div className='card-body'>
+              <div className='title-div'>
+                <h5 className='card-title title-head'>Peppy Paneer &#8377;</h5>
+                <p className='card-text title-subhead'>
+                  Flavorful trio of juicy paneer, crisp capsicum with spicy red
+                  paprika
+                </p>
+              </div>
+              <button
+                type='button'
+                className='btn btn-outline-success float-right'
+                style={{
+                  fontWeight: 'bold',
+                  fontSize: '0.75rem',
+                  padding: '.375rem .5rem'
+                }}
+              >
+                ADD TO CART
+              </button>
+            </div>
+          </div>
+
+
+
+          <div
+            className='card'
+            style={{  
               width: '23rem',
               border: '1px solid #fff',
               boxShadow:
@@ -260,6 +260,22 @@ class MenuPage extends Component {
            */}
 
         </div>
+
+        
+
+        <div className='cart-div'>
+          <div className="card" style={{ width: '21rem', border: '1px solid #fff', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 2px 4px 0px' }}>
+            <div>
+              <img src={emptyCart} className="card-img-right" alt="EMPTY CART" style={{ width: '175px', position: 'absolute', top: '0px', right: '0px' }} />
+            </div>
+            <div className="card-body" style={{ height: '150px', marginTop: '270px' }}>
+              <p className='empty-cart-head'>YOUR CART IS EMPTY</p>
+              <p className='empty-cart-subhead'>Please add some items from the menu.</p>
+            </div>
+          </div>
+        </div>
+
+
       </React.Fragment>
     );
   }
---
+++ dominos-menu-clone/src/data/data.js
@@ -0,0 +1,61 @@
+let pizzas = [{
+    category: 'veg',
+    crust: 'New Hand Tossed',
+    description: 'Flavorful trio of juicy paneer, crisp capsicum with spicy red paprika',
+    favorite: 'false',
+    id: '1',
+    price: '215',
+    size: 'Regular',
+    title: 'Peppy Paneer'
+},
+{
+    category: 'veg',
+    crust: 'New Hand Tossed',
+    description: 'Veg delight - onion, capsicum, grilled mushroom, corn & paneer',
+    favorite: 'false',
+    id: '2',
+    price: '450',
+    size: 'Medium',
+    title: 'Deluxe Veggie'
+},
+{
+    category: 'veg',
+    crust: 'New Hand Tossed',
+    description: 'Black olives, capsicum, onion, grilled mushroom, corn, tomato, jalapeno & extra cheese',
+    favorite: 'false',
+    id: '3',
+    price: '450',
+    size: 'Medium',
+    title: 'Veg Extravaganza'
+},
+{
+    category: 'nonveg',
+    crust: 'New Hand Tossed',
+    description: 'Double pepper barbecue chicken, golden corn and extra cheese, true delight',
+    favorite: 'false',
+    id: '4',
+    price: '450',
+    size: 'Medium',
+    title: 'Chicken Golden Delight'
+},
+{
+    category: 'nonveg',
+    crust: 'New Hand Tossed',
+    description: 'The wholesome flavour of tandoori masala with Chicken tikka, onion, red paprika & mint mayo',
+    favorite: 'false',
+    id: '5',
+    price: '570',
+    size: 'Medium',
+    title: 'Indi Chicken Tikka'
+},
+{
+    category: 'nonveg',
+    crust: 'New Hand Tossed',
+    description: 'A classic American taste! Relish the delectable flavor of Chicken Pepperoni, topped with extra cheese',
+    favorite: 'false',
+    id: '6',
+    price: '570',
+    size: 'Medium',
+    title: 'Chicken Pepperoni'
+}
+];
