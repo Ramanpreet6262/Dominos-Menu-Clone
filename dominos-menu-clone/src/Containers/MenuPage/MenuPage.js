2fec2f5f65dd0b99d4eed0f9f59c9239a627cb7a
Ramanpreet6262 <ramanpreet6262@gmail.com>
2020-03-03 17:33:58 +0530

    added add to cart logic and ui

--- dominos-menu-clone/src/Containers/MenuPage/MenuPage.js
+++ dominos-menu-clone/src/Containers/MenuPage/MenuPage.js
@@ -59,7 +59,6 @@ class MenuPage extends Component {
   };
 
   render() {
-    console.log(this.state.cart);
     if (this.state.data.length === 0) {
       return <Loader />;
     } else {
@@ -115,11 +114,13 @@ class MenuPage extends Component {
                       </p>
                     </div>
                     {pizza.quantity > 0 ? (
-                      <QuantityButton
-                        quantity={pizza.quantity}
-                        onPlusClick={() => this.incQtyHandler(pizza.id)}
-                        onMinusClick={() => this.decQtyHandler(pizza.id)}
-                      />
+                      <div className='card-qty-btn'>
+                        <QuantityButton
+                          quantity={pizza.quantity}
+                          onPlusClick={() => this.incQtyHandler(pizza.id)}
+                          onMinusClick={() => this.decQtyHandler(pizza.id)}
+                        />
+                      </div>
                     ) : (
                       <button
                         type='button'
@@ -183,10 +184,78 @@ class MenuPage extends Component {
                 overflowY: 'scroll',
                 border: '1px solid #fff',
                 borderTop: '3px solid #82BB37',
+                borderRadius: '0px',
                 boxShadow:
                   'rgba(0, 0, 0, 0.12) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 2px 4px 0px'
               }}
-            ></div>
+            >
+              <div className='cart-items'>
+                {this.state.cart.map((pizzaId, index) => (
+                  <div className='cart-item' key={index}>
+                    <img src={peppyPaneer} className='cart-img' alt='pizza' />
+                    <p className='cart-item-title'>
+                      {this.state.data[pizzaId].title}
+                    </p>
+                    <p className='cart-item-desc'>
+                      {this.state.data[pizzaId].description}
+                    </p>
+                    <p className='cart-item-details'>
+                      {this.state.data[pizzaId].size} |{' '}
+                      {this.state.data[pizzaId].crust}
+                    </p>
+                    <div className='cart-qty-btn'>
+                      <QuantityButton
+                        quantity={this.state.data[pizzaId].quantity}
+                        onPlusClick={() =>
+                          this.incQtyHandler(this.state.data[pizzaId].id)
+                        }
+                        onMinusClick={() =>
+                          this.decQtyHandler(this.state.data[pizzaId].id)
+                        }
+                      />
+                    </div>
+                    <div className='cart-item-price'>
+                      <span>
+                        &#8377;{' '}
+                        {this.state.data[pizzaId].price *
+                          this.state.data[pizzaId].quantity}
+                      </span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div
+              className='card'
+              style={{
+                width: '21rem',
+                height: '95px',
+                border: '1px solid rgb(239, 245, 251)',
+                borderRadius: '0px',
+                boxShadow:
+                  'rgba(0, 0, 0, 0.12) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 2px 4px 0px, rgba(0, 0, 0, 0.12) 0px -0.56rem 0.62rem 0px',
+                backgroundColor: 'rgb(239, 245, 251)'
+              }}
+            >
+              <div className='subtotal-div'>
+                <span className='subtotal-head'>Subtotal</span>
+                <span className='subtotal-price'>
+                  &#8377;{' '}
+                  {this.state.cart.reduce(
+                    (totalPrice, pizzaId) =>
+                      totalPrice +
+                      this.state.data[pizzaId].price *
+                        this.state.data[pizzaId].quantity,
+                    0
+                  )}
+                </span>
+              </div>
+              <div>
+                <button className='checkout-btn'>
+                  <span className='checkout-text'>CHECKOUT</span>
+                </button>
+              </div>
+            </div>
           </div>
         </React.Fragment>
       );
